46282ef3ab2e5924ce84eee4484b15a4e1ad83b2
arti0404 <61508935+arti0404@users.noreply.github.com>
2021-07-07 01:27:29 +0530

    Update main_menu.c

--- 3_Implement/src/main_menu.c
+++ 3_Implement/src/main_menu.c
@@ -1,1 +1,38 @@
+#include<stdio.h>
+#include<windows.h>
+#include<conio.h>
+#include<ctype.h>
+#include<string.h>
+#include<stdlib.h>
+/*Function to select choice  to perform the task*/
+main(void)
+{
+	
+  WelcomeScreen();//Use to call WelcomeScreen function
+	Title();//Use to call Title function
+	LoginScreen();//Use to call Menu function
+	
+	
+	
 
+}
+/* ************************************************* Welcome Screen ********************************************* */
+void WelcomeScreen(void) //function for welcome screen
+{
+	
+	printf("\n\n\n\n\n\n\n\t\t\t\t#########################################");
+	printf("\n\t\t\t\t#\t\t WELCOME TO\t\t#");
+	printf("\n\t\t\t\t#   HOSPITAL MANAGEMENT SYSTEM    #");
+	printf("\n\t\t\t\t#########################################");
+	printf("\n\n\n\n\n Press any key to continue......\n");
+	getch();//Use to holds screen for some seconds
+	system("cls");//Use to clear screen
+	
+}
+/* ************************************************* Title Screen ********************************************* */
+void Title(void)//function for title screen
+{
+	printf("\n\n\t\t----------------------------------------------------------------------------------");
+	printf("\n\t\t\t\t       HOSPITAL         ");
+	printf("\n\t\t----------------------------------------------------------------------------------");
+}
